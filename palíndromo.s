7c50a5e2c1de072a8941a97879b5b35c501dfd72
barbaramrr <86504723+barbaramrr@users.noreply.github.com>
2024-11-07 17:36:14 -0300

    Update palíndromo.s

--- palíndromo.s
+++ palíndromo.s
@@ -1,79 +1,75 @@
-## CÓDIGO ASSEMBLY PARA MIPS 32 
-### Palíndromo
-
 	.data
 ms:		.asciiz " "
-tamanho:	.asciiz "Insira o numero de letras da palavra:  "
+tamanho:	.asciiz "Insira o numero de letras da palavra: "
 palavra:	.asciiz "Insira a palavra: "
 true_msg: .asciiz " The string is a palindrome!"
 false_msg: .asciiz " The string is not a palindrome!"
-vetor:   .space 100                                  
+vetor:   .space 100                                   # espaço para a string
 
 	.text
  main:
 
-
 ###################################################################################
  ########### IMPRIME PEDINDO O NUMERO DE LETRAS E GUARDA ESSE VALOR################
- ##################################################################################
-	addi $v0, $zero, 4 	# sys call
-	la $a0, tamanho	                # guarda em $a0 oq precisa ser impresso
-	syscall		                      # Print "Insira o numero de letras""
-	
-	addi $v0,$zero, 5               # ler numero inteiro
-  syscall		 #
-  add $a3, $zero, $v0             # a3 = tamanho 	
-    	
- ##################################################################################
- ########### IMPRIME PEDINDO O NUMERO DE LETRAS E GUARDA ESSE VALOR################
- ##################################################################################
- 
-	addi $v0, $zero, 4          	  # sys call
-	la $a0, palavra                 # guarda em $a0 oq precisa ser impresso
-	syscall	                      	# Print "Insira a palavra""
- 
-	addi $v0,$zero , 8	            # ler o valor inserido
-  la $a0, vetor
-  li $a1, 100                    # Tamanho máximo da string
-  syscall		
-  
- addi $t0,$zero,0                #usado para ler a palavra da esquerda para direita  (i)
- addi $t1, $a3, -1                 #usado para ler a palavra da direita para a esquerda  (j)
- 
+##################################################################################
+	addi $v0, $zero, 4         # sys call para imprimir
+	la $a0, tamanho            # guarda em $a0 o endereço de "Insira o numero de letras"
+	syscall                    # imprime
+
+	addi $v0,$zero, 5          # sys call para ler inteiro
+	syscall                    # lê o número de letras
+	add $a3, $zero, $v0        # a3 = tamanho 
+
+##################################################################################
+ ########### IMPRIME PEDINDO A PALAVRA E GUARDA ESSE VALOR################
+##################################################################################
+	addi $v0, $zero, 4         # sys call para imprimir
+	la $a0, palavra            # guarda em $a0 o endereço de "Insira a palavra"
+	syscall                    # imprime
+
+	addi $v0,$zero , 8         # sys call para ler string
+	la $a0, vetor              # carrega o endereço de vetor
+	li $a1, 100                # tamanho máximo da string
+	syscall                    # lê a string
+
+	addi $t0, $zero, 0         # índice da esquerda (i)
+	addi $t1, $a3, -1          # índice da direita (j) = tamanho - 1
+
 ##########################################################################################
 ################################# WHILE ##################################################
 ##########################################################################################
 WHILE:
- bge $t0,$t1, FIM_WHILE
-    lb $t2, 0($a0)                 # Carrega caractere da esquerda (vetor[i])
-    lb $t3, 0($t1)                 # Carrega caractere da direita (vetor[j])
-    bne $t2,$t3, FALSO
-    addi $t0,$t0,1                  #incrementa i
-    addi $t1, $t1, -1                 # Decrementa j
-    j WHILE
-    
- ##########################################################################################
+	bge $t0, $t1, FIM_WHILE     # Se i >= j, fim do loop (palíndromo)
+
+	lb $t2, 0($a0)              # Carrega caractere de vetor[i]
+	lb $t3, 0($a0)              # Carrega caractere de vetor[j]
+	add $t2, $t2, $t0           # Carrega vetor[i]
+	add $t3, $t3, $t1           # Carrega vetor[j]
+
+	bne $t2, $t3, FALSO         # Se os caracteres não forem iguais, não é palíndromo
+
+	addi $t0, $t0, 1            # Incrementa i
+	addi $t1, $t1, -1           # Decrementa j
+	j WHILE
+
+##########################################################################################
 ################################# FIM WHILE ###############################################
 ##########################################################################################
+FIM_WHILE:
+	addi $v0, $zero, 4          # sys call para imprimir
+	la $a0, true_msg            # mensagem: "A palavra é palíndromo"
+	syscall                     # imprime
+	j end
 
- FIM_WHILE:
- 	addi $v0, $zero, 4          	  # sys call
-	la $a0, true_msg                 # guarda em $a0 oq precisa ser impresso
-	syscall	                      	# Print "Insira a palavra""
- j end
-
-     
- ##########################################################################################
-#################################   FALSO     ############################################
 ##########################################################################################
- 
+################################# FALSO ##################################################
+##########################################################################################
 FALSO:
- 	addi $v0, $zero, 4          	  # sys call
-	la $a0, false_msg                 # guarda em $a0 oq precisa ser impresso
-	syscall	
-  j end
+	addi $v0, $zero, 4          # sys call para imprimir
+	la $a0, false_msg           # mensagem: "A palavra não é palíndromo"
+	syscall                     # imprime
+	j end
 
-  end:
-    li $v0, 10                     # Syscall para encerrar o programa
-    syscall
-    	
+end:
+	li $v0, 10                  # sys call para encerrar o programa
+	syscall
